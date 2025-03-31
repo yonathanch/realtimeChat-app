eccf02eb7fc62607880f08b8c7977609e77826ae
yonathan <chyonatan18@gmail.com>
2025-03-31 13:47:28 +0700

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,18 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [tailwindcss(), react()],
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [react()],
+  build: {
+    outDir: "dist", // Pastikan Vite membangun proyek di folder dist
+  },
 });
